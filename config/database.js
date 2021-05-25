eaf1b4f08f874844a76c11509907c09b129c19de
José R. Ferreyra <joserferreyra@gmail.com>
2021-05-25 20:55:09 -0300

    Conexión a DB producción

--- config/database.js
+++ config/database.js
@@ -2,8 +2,8 @@ module.exports = {
   dbPool: {
     user: 'US_SUELDO',
     password: 'usuario',
-    connectString: 'www.sueldos.duckdns.org:1521/DESA',
-    //connectString: 'www.sueldos.duckdns.org:1521/ORCL',
+    //connectString: 'www.sueldos.duckdns.org:1521/DESA',
+    connectString: 'www.sueldos.duckdns.org:1521/ORCL',
     //connectString: '192.168.1.8:1521/ORCL',
     poolMin: 10,
     poolMax: 10,
