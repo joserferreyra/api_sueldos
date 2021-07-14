cf3160d66c8fce5ab74bc5b9729804f2bd36594f
José R. Ferreyra <joserferreyra@gmail.com>
2021-07-13 22:02:42 -0300

    Inicio Rama Producción

--- config/web_server.js
+++ config/web_server.js
@@ -1,5 +1,5 @@
 module.exports = {
-    port: process.env.HTTP_PORT || 3001,
-    host: '192.168.0.102'
-    //host: '0.0.0.0'
+    port: process.env.HTTP_PORT || 3003,
+    //host: '192.168.0.102'
+    host: '0.0.0.0'
 };
